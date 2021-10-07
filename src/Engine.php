d46a9a54d14844460234623e7044027e7d0d22f5
Aslan Autlev <autlevaslan@gmail.com>
2021-10-07 15:40:58 +0300

    изменил движок

--- src/Engine.php
+++ src/Engine.php
@@ -5,29 +5,34 @@ namespace Brain\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function engineGame($gameData)
+function engineGame(array $gameData)
 {
     line("Welcome to the Brain Game!");
+//    $name = line("May I have your name?");
     $name = prompt("May I have your name?");
     line("Hello, %s!", $name);
     line($gameData['startMessage']);
-    for ($i = 1; $i <= $gameData['countGame']; $i++) {
-        line("Question: {$gameData[$i][0]}");
-        $answerUser = prompt("Your answer");
-        if ($answerUser === "yes" && $gameData[$i][1]) {
-            line("Correct!");
-        } elseif ($answerUser === "no" && !$gameData[$i][1]) {
-            line("Correct!");
-        } else {
-            switch ($gameData[$i][1]) {
-                case true:
-                    line("'$answerUser' is wrong answer ;(. Correct answer was '{$gameData[$i][2]}'.\nLet's try again, $name!");
-                    exit;
-                case false:
-                    line("'$answerUser' is wrong answer ;(. Correct answer was '{$gameData[$i][2]}'.\nLet's try again, $name!");
-                    exit;
-            }
-        }
-    }
+    rounds($gameData, $name);
     line("Congratulations, $name!");
+}
+
+function rounds(array $gameData, string $name)
+{
+    for ($i = 1; $i <= $gameData['countGame']; $i++) {
+        line("Question: {$gameData[$i]['question']}");
+//        $answerUser = line("Your answer");
+        $answerUser = prompt("Your answer");
+        reply($answerUser, $i, $gameData, $name);
+    }
+}
+
+function reply(string $answerUser, int $i, array $gameData, string $name)
+{
+    if ($answerUser === $gameData[$i]['answer']) {
+        line("Correct!");
+    } else {
+        line("'$answerUser' is wrong answer ;(. Correct answer was '{$gameData[$i]['answer']}'.");
+        line("Let's try again, $name!");
+        exit;
+    }
 }
