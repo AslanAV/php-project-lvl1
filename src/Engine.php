ea5209c9d34f674913861d69d735b017030ea1c4
Aslan <autlevaslan@yandex.ru>
2022-06-27 12:43:07 +0300

    some fix

--- src/Engine.php
+++ src/Engine.php
@@ -8,7 +8,7 @@ use function cli\prompt;
 const ROUNDS_COUNT = 3;
 function engineGame(array $gameData, string $startMessage): void
 {
-    line("Welcome to the Brain Games!");
+    line("/Welcome to the Brain Games!/");
     $name = prompt("May I have your name?");
     line("Hello, %s!", $name);
     line($startMessage);
